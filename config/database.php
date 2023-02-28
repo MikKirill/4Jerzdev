f755cdd1b4b0848d52a467f934b3016366bfc5a7
MikKirill <94719239+MikKirill@users.noreply.github.com>
2023-02-28 23:58:36 +0300

    updating

--- config/database.php
+++ config/database.php
@@ -7,10 +7,10 @@ require '../vendor/autoload.php';
 class Database
 {
 
-    private string $host = 'localhost';
-    private string $db_name = 'test_schoolX';
+    private string $host = '127.0.0.1';
+    private string $db_name = 'test';
     private string $username = 'root';
-    private string $password = 'cbgbyfqy';
+    private string $password = 'jkljkljkl';
 
     public ?PDO $conn = null;
 
@@ -19,13 +19,15 @@ class Database
 
         try {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
-
+            $this->conn->exec("set names utf-8");
+            $statement = $this->conn->query('SELECT * FROM names');
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                echo $row['id'] . ' ' . $row['name'] . ' ' . $row['username'] . ' ' . $row['city_id'];
+            }
         } catch (PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
 
         return $this->conn;
     }
-
 }
